2cedfec6311c535f96bd25c2fa6cc0f970e9b89e
방주영 <rachel@bangjuyeong-ui-MacBookPro.local>
2024-03-13 21:25:26 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const moreBTN = document. querySelector('.info .metadata .moreBTN');
+const title = document. querySelector('.info .metadata .title');
+
+moreBTN.addEventListener('click', () => {
+    moreBTN.classList.toggle('clicked');
+    title.classList.toggle('clamp')
+});
+
+
