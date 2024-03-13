f80539b17af291ab4d7387c62be5fda5fd5c5045
joo93 <joo93d.o@gmail.com>
2024-03-13 22:32:09 +0900

    정리정돈

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const moreBTN = document. querySelector('.info .metadata .moreBTN');
-const title = document. querySelector('.info .metadata .title');
+const moreBTN = document.querySelector(".info .metadata .moreBTN");
+const title = document.querySelector(".info .metadata .title");
 
-moreBTN.addEventListener('click', () => {
-    moreBTN.classList.toggle('clicked');
-    title.classList.toggle('clamp')
+moreBTN.addEventListener("click", () => {
+  moreBTN.classList.toggle("clicked");
+  title.classList.toggle("clamp");
 });
-
-
